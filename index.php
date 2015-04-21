044400361b7ce9479ae90fb74f41a0070d8ebb6b
IvesShen <870154211@qq.com>
2015-04-21 11:15:37 +0800

    ives 1 edition

--- index.php
+++ index.php
@@ -3,24 +3,35 @@
 <?php
 echo "<link rel='stylesheet' href='css/main.css''>";
 ?>
-<head>
 
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="description" content="Typing Game is a online game which can help you improve your typing skills. " />
+	<meta name="keywords" content="Typing Game, Typing skills, Razer" />
+	<meta name="author" content="Razer" />
+	<meta name="Copyright" content="Razer" />
+	<meta name="robots" content="index, follow" />
 </head>
 <body>
 
 <div id="ConText">
-
-</div>
+	<div id="Welcome"><p> Welcome to Typing Game! </p></div>
+	<div id="Play"><a href="play.index"><img src="" alt="PLAY"/></div>
+</div><!--end ConText-->
 <div id="NavBar">
-    <?php
-
-    ?>
-</div>
+	<div id="LOGO"><h1><a href="index.html"><img src="img/logo_adflavor_footer.gif" alt="Typing Game" /></a></h1></div>
+    <div class="menuitems"><a href="game.php">Game</a></div>
+    <div class="menuitems"><a href="userprofile.html">UserProfile</a></div>
+   
+</div><!--end NacBar-->
 <div id="Address">
-    <?php
+    <div class="footer"><a href="index.html"></div>
+        <p>@ 2015 Razer. All rights reserved.</p><p><a href="about.html"target="top">About</a></p>
+ 
 
-    ?>
-</div>
+   
+</div><!--end Address-->
+
 </body>
 </html>
 <?php
