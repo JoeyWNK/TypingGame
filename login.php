888dc40be5ae97067d2c4b1a4a0eb7017c8a23ae
WNK1993 <wnk.1993@live.com>
2015-05-27 18:20:14 +0800

    add ajax tech

--- login.php
+++ login.php
@@ -1,67 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<meta name="description" content="Typing Game is a online game which can help you improve your typing skills. " />
-	<meta name="keywords" content="Typing Game, Typing skills, Razer" />
-	<meta name="author" content="Razer" />
-	<meta name="Copyright" content="Razer" />
-	<meta name="robots" content="index, follow" />
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/login.css">
-	<script language="JavaScript" src="js/nav.js"></script>
-        <script language="JavaScript" src="js/jquery-1.11.2.min.js"></script>
-</head>
-
-
-<body>
-
-<div id="ConText">
-    <link rel="stylesheet" href="css/index.css"></div>
-  <div id="NavBar">
-    <div id="logo">
-       <a href="#">
-            <img src="images/logo.png" alt="Typing Game">
-        </a>
-    </div>
-    <ul id="nav">
-        <li><a href="game/index.php"title="Typing Game">Game</a></li>
-        <li><a href="user/index.php"title="Typing Game">History</a></li>
-        <li><a href="about/index.php"title="Typing Game">About</a></li>
-        <li><a href="login.php"title="Typing Game">Log in</a></li>
-    </ul>
-
-</div><!--end NavBar-->
-
-  <div id="login_bg" class ="login_bg" style="background-image:url(images/007.jpg);"></div>
-    
-    <div class="container">
-    	<div class="signup_forms">
-        <div id="signup_forms_container" class="signup_forms_container">
-           <form class="signup_form_form" id="sign_form" method="post" action="*.php">
-                <div class="signup_account" id="signup_account">
-                <div class="form_user">	
-             <span class="form_user">
-         <input type="text" name="Username" placeholder="Username" id="signup_username">
-             </span>
-            </div>         	
-          <div class="form_password">
-          <input type="password" name="password" placeholder="Password" id="signup_password">
-           </div>
-          </div>
-         </div>                
-         <input type="submit" id="signup_forms_submit"value="Log in">     
-         <input type="submit" id="signup_forms_submit"value="Register">
-                   
-           </div>
-          </form>
-         </div>
-        </div>
-<div id="Address">
-    <div class="footer">
-        <p>@ 2015 Razer. All rights reserved.</p>
-    </div>
-</div><!--end Address-->
-
-</body>
-</html>
+<?php 
+if (isset($_POST['action'])){
+if ($_POST['action'] === 'up'){
+if (USERCHECK($_POST['name'])){
+if (USERREGISTER($_POST['name'],$_POST['pass'])){
+die ('{"re":true,"action":"up"}');
+}
+}
+} else if ($_POST['action'] === 'in'){
+$uid = USERLOGIN($_POST['name'],$_POST['pass']);
+if (isset($uid)){
+die ('{"re":true,"action":"in"}');
+}
+}
+die ('{"re":false"}');
+}
