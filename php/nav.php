351686e3951af9c497882ab6326036c01959731a
Your Name <632867957@qq.com>
2015-06-04 10:47:06 +0800

    fix mistake on nav and about path

--- php/nav.php
+++ php/nav.php
@@ -10,10 +10,10 @@
        echo'<li><a href=../game>GAME</a></li>';
        echo'<li><a href=../about>ABOUT</a></li>';
        echo'<li><a href=../user>USER</a></li>';
-       echo'<li><a href="#" id="logoutBtn">lOG OUT('.$_SESSION['username'].')</a></li>';
+       echo'<li><a href="#" id="logoutBtn">('.$_SESSION['username'].')</a></li>';
    }else {
        echo'<li><a href=../game>GAME</a></li>';
        echo'<li><a href=../about>ABOUT</a></li>';
        echo'<li><a href=../user>USER</a></li>';
-       echo'<li><a href="#" id="loginBtn">lOG IN</a></li>';
+       echo'<li><a href="../" id="loginBtn">lOG IN</a></li>';
    }
